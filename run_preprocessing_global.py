2fd49a951837fa37574fc9dcb4f8571a8c2db9d7
Federica Di Lauro <federicadilauro1998@gmail.com>
2023-11-08 17:29:30 +0100

    Add option to use voxel_size based on the specific dataset

--- run_preprocessing_global.py
+++ run_preprocessing_global.py
@@ -1,17 +1,19 @@
 import os, shutil
 from multiprocessing import Pool
 from pathlib import Path
+import numpy as np
+
 PY3="python3"
 
 GPU = "0"
 os.environ["CUDA_VISIBLE_DEVICES"] = GPU
 
 BENCHMARK_DIR="/benchmark/point_clouds_registration_benchmark/"
-PREPROCESSING_DIR="/benchmark/experiments/RoReg/preprocessing"
-VOXEL_SIZE = 0.1
+PREPROCESSING_DIR="/benchmark/experiments/RoReg/preprocessing_dynamic_voxel"
+VOXEL_SIZE = "dynamic"
+VOXEL_DIR = "/benchmark/experiments/RoReg/voxel/"
 
-base_command = ( f'{PY3}' + ' testset_benchmark.py ' +
-                 f'--voxel_size={VOXEL_SIZE}')
+base_command = (f'{PY3}' + ' testset_benchmark.py ')
 
 
 problem_txts = ['kaist/urban05_global.txt',
@@ -49,7 +51,15 @@ pcd_dirs = ['kaist/urban05/',
 commands = []
 
 for problem_txt, pcd_dir in zip(problem_txts, pcd_dirs):
+    if VOXEL_SIZE == "dynamic":
+        problem_name = os.path.splitext(os.path.basename(problem_txt))[0]
+        f = os.path.join(VOXEL_DIR, problem_name+".npy")
+        voxel = np.load(f)
+        print(f'Running experiments on {problem_name} using voxel size {voxel}')
+    else:
+        voxel = VOXEL_SIZE
     full_command = (base_command +
+                    f'--voxel_size={voxel}'
                     f' --input_txt={BENCHMARK_DIR}/{problem_txt}' +
                     f' --pcd_dir={BENCHMARK_DIR}/{pcd_dir}' +
                     f' --out_dir={PREPROCESSING_DIR}/{pcd_dir}')
